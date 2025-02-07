0c4419c2448e66e032970c3be269057da288239a
AfeezTemitope <belloafeez28@gmail.com>
2025-02-07 18:01:30 +0100

    created user and contact repositories

--- src/main/java/org/example/data/models/Contacts.java
+++ src/main/java/org/example/data/models/Contacts.java
@@ -1,10 +1,12 @@
 package org.example.data.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Contacts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String phoneNumber;
     @ManyToOne
